3771e95e390550515d55b387efe5221c415f9e10
juvarabrera <juvar.abrera2@gmail.com>
2016-06-26 13:15:04 +0800

    Fixed popup js

--- scripts/App.js
+++ scripts/App.js
@@ -88,9 +88,9 @@ var App = {
 				"opacity": "1"
 			}, 500)
 			$popup.html('<div class="loading"><svg class="circular" viewBox="25 25 50 50"><circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="6" stroke-miterlimit="10"/></svg></div>');
+			$popup.css("width", "1px");
 			var width = $popup.outerWidth(), height = $popup.outerHeight();
 			$popup.css({
-				"width": "1px",
 				"top": "50%",
 				"left": "50%",
 				"margin-top": 0-(height/2)+"px",
